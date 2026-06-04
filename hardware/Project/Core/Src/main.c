b7fa181191c3dbbe5cecd0d344312d98dd21e6bb
SandBOOM <3263401045@qq.com>
2026-06-04 21:34:53 +0800

    feat:添加cubemx中对于usart1的配置

--- hardware/Project/Core/Src/main.c
+++ hardware/Project/Core/Src/main.c
@@ -20,6 +20,7 @@
 #include "main.h"
 #include "i2c.h"
 #include "tim.h"
+#include "usart.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -96,6 +97,7 @@ int main(void)
   MX_I2C1_Init();
   MX_I2C2_Init();
   MX_TIM3_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
   OLED_Init();
   OLED_Clear();
